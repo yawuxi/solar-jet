13c96757328243eb60dfe8602ac477b7eb4744c4
Dmytro ryliov.work@gmail.com Rylov <ryliov.work@gmail.com>
2023-06-07 11:34:56 +0200

    fix: lzay loading suspense component

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,16 @@
 import * as React from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
+import { Suspense } from "react";
 import { App } from "./App";
 import "./index.module.scss";
 
 createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <Suspense fallback="Loading...">
+        <App />
+      </Suspense>
     </BrowserRouter>
   </React.StrictMode>
 );
